94af977e9ee680e4c68b6ecc72a392f071e45c76
D1MonL1Mon <limon.cheat@mail.ru>
2021-03-30 12:56:34 +0300

    Final

---
+++ src/Window.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class Window extends JFrame {
+
+    public Window() {
+        setTitle("Змейка");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setSize(320, 345);
+        setLocation(400, 400);
+        add(new GameField());
+        setVisible(true);
+    }
+}
